7eb204ecc4990c7cde71ff79aecb3e12fb31e25b
Stepa0425 <karapetyanstepa@gmail.com>
2024-11-21 19:34:27 +0300

    create Range and configure db connection

---
+++ src/main/java/by/agsr/MonitorSensors/models/Range.java
@@ -0,0 +1,38 @@
+package by.agsr.MonitorSensors.models;
+
+
+import jakarta.persistence.Id;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Column;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "ranges")
+public class Range {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @NotNull(message = "Поле 'from' не может быть пустым")
+    @Positive(message = "Поле 'from' должно быть положительным целым числом")
+    private Integer from;
+
+    @NotNull(message = "Поле 'to' не может быть пустым")
+    @Positive(message = "Поле 'to' должно быть положительным целым числом")
+    private Integer to;
+}
