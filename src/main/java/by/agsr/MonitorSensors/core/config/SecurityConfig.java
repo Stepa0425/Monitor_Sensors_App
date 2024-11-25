5b9d2af9a31fd5c900f8f079d851b6f1ec56bb65
Stepa0425 <karapetyanstepa@gmail.com>
2024-11-25 12:21:22 +0300

    add security app

---
+++ src/main/java/by/agsr/MonitorSensors/core/config/SecurityConfig.java
@@ -0,0 +1,49 @@
+package by.agsr.MonitorSensors.core.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractAuthenticationFilterConfigurer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> {
+                    auth.requestMatchers(HttpMethod.GET, "/agsr/api/sensors/**").authenticated();
+                    auth.requestMatchers(HttpMethod.POST, "/agsr/api/sensors/**").hasRole("Administrator");
+                    auth.requestMatchers(HttpMethod.PUT, "/agsr/api/sensors/**").hasRole("Administrator");
+                    auth.requestMatchers(HttpMethod.DELETE, "/agsr/api/sensors/**").hasRole("Administrator");
+                })
+                .formLogin(AbstractAuthenticationFilterConfigurer::permitAll)
+                .logout(LogoutConfigurer::permitAll)
+                .build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    @Primary
+    public AuthenticationManagerBuilder auth(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("admin").password(passwordEncoder().encode("admin_password")).roles("Administrator")
+                .and()
+                .withUser("user").password(passwordEncoder().encode("user_password")).roles("Viewer");
+        return auth;
+    }
+}
--- src/main/java/by/agsr/MonitorSensors/rest/SensorController.java
+++ src/main/java/by/agsr/MonitorSensors/rest/SensorController.java
@@ -1,7 +1,6 @@
 package by.agsr.MonitorSensors.rest;
 
 import by.agsr.MonitorSensors.core.dto.SensorRequestDTO;
-import by.agsr.MonitorSensors.core.dto.SensorResponseDTO;
 import by.agsr.MonitorSensors.core.services.SensorService;
 import jakarta.validation.Valid;
 
@@ -12,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -22,18 +22,17 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.List;
-
 
 @RestController
-@RequestMapping("/agsr/monitor/sensors")
+@RequestMapping("/agsr/api")
 @RequiredArgsConstructor(access = AccessLevel.PACKAGE)
 public class SensorController {
 
     @Autowired
     private final SensorService sensorService;
 
-    @PostMapping(path = "/",
+    @PreAuthorize("hasRole('Administrator')")
+    @PostMapping(path = "/sensors",
             consumes = "application/json",
             produces = "application/json")
     public ResponseEntity<?> createSensor(@RequestBody @Valid SensorRequestDTO sensorRequestDTO) {
@@ -41,19 +40,22 @@ public class SensorController {
         return ResponseEntity.status(HttpStatus.CREATED).body(createdSensor);
     }
 
-    @GetMapping(path = "/", produces = "application/json")
+    @PreAuthorize("hasAnyRole('Administrator', 'Viewer')")
+    @GetMapping(path = "/sensors", produces = "application/json")
     public ResponseEntity<?> getAllSensors() {
         var sensors = sensorService.getAllSensors();
         return ResponseEntity.ok().body(sensors);
     }
 
-    @DeleteMapping(path = "/{id}")
+    @PreAuthorize("hasRole('Administrator')")
+    @DeleteMapping(path = "/sensors/{id}")
     public ResponseEntity<?> deleteSensor(@PathVariable("id") Long sensorId) {
         sensorService.deleteSensor(sensorId);
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 
-    @PutMapping(path = "/{id}",
+    @PreAuthorize("hasRole('Administrator')")
+    @PutMapping(path = "/sensors/{id}",
             consumes = "application/json",
             produces = "application/json")
     public ResponseEntity<?> updateSensor(@PathVariable("id") Long sensorId,
@@ -62,18 +64,17 @@ public class SensorController {
         return ResponseEntity.status(HttpStatus.OK).body(updatedSensor);
     }
 
-    @GetMapping(produces = "application/json")
-    public ResponseEntity<?> findSensorsByName(@RequestParam(value = "name", required = false) String sensorName,
-                                               @RequestParam(value = "model", required = false) String sensorModel) {
-        List<SensorResponseDTO> foundSensors;
+    @PreAuthorize("hasAnyRole('Administrator', 'Viewer')")
+    @GetMapping(path = "/sensors/search/byName", produces = "application/json")
+    public ResponseEntity<?> findSensorsByName(@RequestParam(value = "name") String sensorName){
+          var foundSensors = sensorService.getByName(sensorName);
+        return ResponseEntity.status(HttpStatus.OK).body(foundSensors);
+    }
 
-        if (sensorName != null && !sensorName.isBlank()) {
-            foundSensors = sensorService.getByName(sensorName);
-        } else if (sensorModel != null && !sensorModel.isBlank()) {
-            foundSensors = sensorService.getByModel(sensorModel);
-        } else {
-            foundSensors = sensorService.getAllSensors();
-        }
+    @PreAuthorize("hasAnyRole('Administrator', 'Viewer')")
+    @GetMapping(path = "/sensors/search/byModel", produces = "application/json")
+    public ResponseEntity<?> findSensorsByModel(@RequestParam(value = "model") String sensorModel){
+        var foundSensors = sensorService.getByModel(sensorModel);
         return ResponseEntity.status(HttpStatus.OK).body(foundSensors);
     }
 }
